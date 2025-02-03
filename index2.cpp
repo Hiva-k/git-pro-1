087936e0ecc5f7ad74a7600512d222449599a669
Hiva Kameli <hivakameli22@gmail.com>
2025-02-03 16:36:54 +0330

    index lol

--- index2.cpp
+++ index2.cpp
@@ -3,7 +3,11 @@ using namespace std;
 int main (){
     cout << "hello";
     cout << "hi";
-    int a;
+    int a ,b , f;
+    cout << "salam:";
+    cout << "lol";
+    cin >> b;
     cin >> a;
+    cin >> f;
     return 0;
 }
